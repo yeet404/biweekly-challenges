5451541ad397ec3402152cabc2122f48d191bdba
= <93748878+yeet404@users.noreply.github.com>
2021-12-24 10:16:21 -0500

    Create add and remove methods

--- week4/main-w4.py
+++ week4/main-w4.py
@@ -6,13 +6,43 @@ class SeatLayout:
         for i in range(self.w):
             self.layout.append([0 for j in range(self.l)])
 
+    def add(self, row, col):
+        row -= 1
+        col -= 1
+        try:
+            if not self.layout[row][col]:
+                self.layout[row][col] = 1
+            else:
+                print("Seat occupied.")
+        except IndexError:
+            print("Invalid seat.")
+
+    def remove(self, row, col):
+        row -= 1
+        col -= 1
+        try:
+            if self.layout[row][col]:
+                self.layout[row][col] = 0
+            else:
+                print("Seat empty.")
+        except IndexError:
+            print("Invalid seat.")
+
     def print(self):
         for i in range(self.w):
             print(*self.layout[i])
 
 def main():
-    sl = SeatLayout(3, 4)
-    sl.print()
+    test = SeatLayout(3, 4)
+    test.add(4, 3) # add
+    test.add(3, 3) # add+remove
+    test.remove(3, 3)
+    test.add(4, 3) # add occupied
+    test.remove(1, 1) # remove empty
+    test.add(100, 100) # add invalid
+    test.remove(1000, 34) # remove invalid
+    
+    test.print()
 
 if __name__ == "__main__":
     main()
